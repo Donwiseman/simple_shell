4c3bdd0cca686c499af37a0eeb645a0f17eba4c2
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-04-26 17:47:32 -0700

    implementation the the shell.c which is the main function for the simple shell project

---
+++ shell.c
@@ -0,0 +1,121 @@
+#include "shell.h"
+#include <string.h>
+
+/**
+ * execute_command - executes a given program from the argument vector
+ * @sh_name: name which the shell program was called
+ * @argv: argument vector for the argument to be excuted
+ * @count: the current count of the programs sthe shell has executed
+ *
+ * Return: 0 if no errors otherwise -1
+ */
+int execute_command(char *sh_name, char **argv, unsigned int count)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		errors(sh_name, argv[0], count);
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		if ((execve(argv[0], argv, environ)) < 0)
+		{
+			errors(sh_name, argv[0], count);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+		if (!WIFEXITED(status))
+			return (-1);
+	}
+	return (0);
+}
+
+/**
+ * special_case - handles cases when command is not a relative or absolute path
+ * @sh_name: name the shell was called for error printing
+ * @argv: argument vector
+ * @count: pointer to the counter for programs executed by the shell
+ *
+ * Return: 0 if program should continue normally, 1 if loop should be restarted
+ * and 2 if loop should be broken (i.e exit)
+ */
+int special_case(char *sh_name, char **argv, int count)
+{
+	char *name;
+
+	if (_strncmp(argv[0], "exit", 5) == 0)
+		return (2);
+	/**
+	 * else if ((_strncmp(argv[0], "env", 4) == 0)
+	 * {env();
+	 * return (1);}
+	 * else if ((_strncmp(argv[0], "cd", 3) == 0)
+	 * {cd(argv[1]);
+	 * return (1);}
+	 */
+	name = path(argv[0]);
+	if (name == NULL)
+	{
+		errors(sh_name, argv[0], count);
+		return (1);
+	}
+	else
+	{
+		free(argv[0]);
+		argv[0] = name;
+	}
+	return (0);
+}
+
+/**
+ * main - runs the simple shell program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 if executes with no error, otherwise 1
+ */
+int main(int ac, char **av)
+{
+	char **argv, buf[BUFSIZE], *sh_name = av[0];
+	char *buffer = buf;
+	unsigned int count = 0, feed;
+	size_t n = BUFSIZE;
+
+	if (ac > 1)
+	{
+		errors(sh_name, av[1], count);
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		if ((getline(&buffer, &n, stdin)) < 1)
+			continue;
+		argv = token(buffer);
+		if (argv == NULL)
+			continue;
+		if (argv[0][0] != '/' && argv[0][0] != '.')
+		{
+			feed = special_case(sh_name, argv, ++count);
+			if (feed == 2)
+				break;
+			else if (feed == 1)
+				continue;
+			/*no function/program executed and only the path used*/
+			count--;
+		}
+		execute_command(sh_name, argv, ++count);
+		if ((isatty(STDIN_FILENO)) == 0)
+			break;
+		free_arg(argv);
+	}
+	free_arg(argv);
+	return (0);
+}
