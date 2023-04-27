49a4c75a82724e92eb261d2499d4a3534f32bc90
“Bizcom56” <dornorjoshua56@gmail.com>
2023-04-27 16:15:40 -0700

    [UPDATED]

--- shell.h
+++ shell.h
@@ -16,6 +16,36 @@
 #define TOK_BUFSIZE 128
 #define TOK_DELIM " \t\r\n\a"
 
+typedef struct data
+{
+        char **av;
+        char *input;
+        char **args;
+        int status;
+        int counter;
+        char **_environ;
+        char *pid;
+} data_shell;
+
+/* envrn1.c  */
+char *_getenv(const char *name, char **_environ);
+int _env(data_shell *datash);
+
+/* envrn1.c */
+char *copy_info(char *name, char *value);
+void set_env(char *name, char *value, data_shell *datash);
+int _setenv(data_shell *datash);
+int _unsetenv(data_shell *datash);
+
+/* cd.c */
+void cd_dot(data_shell *datash);
+void cd_to(data_shell *datash);
+void cd_previous(data_shell *datash);
+void cd_to_home(data_shell *datash);
+
+/* cd_shell.c */
+int cd_shell(data_shell *datash);
+
 
 /* Points to an array of pointers to strings called the "environment" */
 extern char **environ;
